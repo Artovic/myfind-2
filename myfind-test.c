c378e73e47d756e57fa4ddd30792fdcb1ee6c20a
Leandros Athanasiadis <leandre84@gmail.com>
2014-03-01 18:00:22 +0100

    asd

--- myfind-test.c
+++ myfind-test.c
@@ -30,6 +30,9 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <pwd.h>           /** PW Library **/
+#include <grp.h>           /** Gruppenstruktur **/
+#include <time.h>
 
 
 /*
@@ -41,6 +44,9 @@
 /*
  * -------------------------------------------------------------- typedefs --
  */
+
+typedef enum boolean { false, true } boolean_t;
+
 
 /*
  * --------------------------------------------------------------- globals --
@@ -54,6 +60,8 @@ void usage(const char * const * myname);
 void do_file(const char * file_name, const char * const * params);
 void do_dir(const char * dir_name, const char * const * params);
 char get_file_type(const struct stat * file);
+void ls(const struct stat * file, const char * file_name);
+boolean_t nouser(const struct stat * file);
 
 /**
  *
@@ -91,6 +99,8 @@ void do_file(const char * file_name, const char * const * params) {
 
 	struct stat myfile;
 
+	params = params;
+
 	#ifdef MYFIND_DEBUG
 	fprintf(stderr, "do_file was called for file: %s\n", file_name);
 	#endif
@@ -99,9 +109,8 @@ void do_file(const char * file_name, const char * const * params) {
 		fprintf(stderr, "Could not stat %s\n", file_name);
 	}
 	else {
-		printf("%s   size: %lu type: %c\n", file_name, myfile.st_size, get_file_type(&myfile));
+		ls(&myfile, file_name);
 
-		/* descend in hierarchy */
 		if ( get_file_type(&myfile) == 'd' ) {
 			do_dir(file_name, params);
 		}
@@ -112,6 +121,7 @@ void do_dir(const char * dir_name, const char * const * params) {
 
 	DIR *mydirp = NULL;
 	struct dirent *thisdir = NULL;
+	char path[1024] = {0};
 
 	#ifdef MYFIND_DEBUG
 	fprintf(stderr, "do_dir was called for dir: %s\n", dir_name);
@@ -127,8 +137,10 @@ void do_dir(const char * dir_name, const char * const * params) {
 		while ( (thisdir = readdir(mydirp)) != NULL ) {
 			/* prevent infinite loops */
 			if ( (strcmp(thisdir->d_name, ".") != 0 ) && (strcmp(thisdir->d_name, "..") != 0 ) ) {
-				/* do_file(thisdir->d_name, params); */
-				do_file(strcat(strcat((char*) dir_name,"/"),thisdir->d_name), params);
+				strcpy(path, dir_name);
+				strcat(path, "/");
+				strcat(path, thisdir->d_name);
+				do_file(path, params);
 			}
 		}
 
@@ -153,6 +165,121 @@ char get_file_type(const struct stat * file) {
 	if(S_ISSOCK(file->st_mode)) { retval = 's'; }
 
 	return retval;
+}
+
+void ls(const struct stat * file, const char * file_name) {
+
+	char permissions[10] = {0};
+	char timestring[30] = {0};
+	struct passwd *pwd = NULL;
+	struct group *grp = NULL;
+	struct tm *ptime = NULL;
+
+	/* initialize permissions with - */
+	memset(permissions, '-', 9 );
+
+	/* user permissions */
+	if ( file->st_mode & S_IRUSR ) { permissions[0] = 'r'; };
+	if ( file->st_mode & S_IWUSR ) { permissions[1] = 'w'; };
+        if ( file->st_mode & S_ISUID ) {
+		if ( file->st_mode & S_IXUSR ) {
+			permissions[2] = 's';
+		}
+		else {
+			permissions[2] = 'S';
+		}
+	}
+	else {
+		if ( file->st_mode & S_IXUSR ) { permissions[2] = 'x'; };
+	}
+	
+	/* group permissions */
+	if ( file->st_mode & S_IRGRP ) { permissions[3] = 'r'; };
+	if ( file->st_mode & S_IWGRP ) { permissions[4] = 'w'; };
+        if ( file->st_mode & S_ISGID ) {
+		if ( file->st_mode & S_IXGRP ) {
+			permissions[5] = 's';
+		}
+		else {
+			permissions[5] = 'S';
+		}
+	}
+	else {
+		if ( file->st_mode & S_IXGRP ) { permissions[5] = 'x'; };
+	}
+
+	/* all permissions */
+	if ( file->st_mode & S_IROTH ) { permissions[6] = 'r'; };
+	if ( file->st_mode & S_IWOTH ) { permissions[7] = 'w'; };
+        if ( file->st_mode & S_ISVTX ) {
+		if ( file->st_mode & S_IXGRP ) {
+			permissions[8] = 't';
+		}
+		else {
+			permissions[8] = 'T';
+		}
+	}
+	else {
+		if ( file->st_mode & S_IXGRP ) { permissions[8] = 'x'; };
+	}
+
+
+	/* print inodenumber */
+	printf("%lu ", file->st_ino);
+	/* print blockcount as 1k blocks*/
+	printf("%lu ", (file->st_blocks / 2));
+	/* print type and permissions */
+	printf("%c%s ", get_file_type(file), permissions);
+	/* print hardlink count */
+	printf("%lu ", file->st_nlink);
+
+	/* lookup name for UID */
+	if ( (pwd = getpwuid(file->st_uid)) == NULL) {
+		printf("%d ", file->st_uid);
+	}
+	else {
+		printf("%s ", pwd->pw_name);
+	}
+
+	pwd = NULL;
+
+	/* lookup name for GID */
+        grp = getgrgid(file->st_gid);
+
+        if (grp == NULL) {
+                printf("%d ", file->st_gid);
+        }
+        else {
+                printf("%s ", grp->gr_name);
+        }
+
+        grp = NULL;
+
+	/* print file size */
+        printf("%lu ", file->st_size);
+
+	/* parse st_mtime, write it into timestring and print it */
+	ptime = localtime(&file->st_mtime);
+	strftime(timestring,30,"%b %d %H:%M %Y", ptime);
+	printf("%s ", timestring);
+
+	/* print file name */
+	printf("%s\n", file_name);
+
+}
+
+boolean_t nouser(const struct stat * file) {
+
+	struct passwd *pwd = NULL;
+
+	if ( (pwd = getpwuid(file->st_uid)) == NULL) {
+		return true;
+	}
+	else {
+		pwd = NULL;
+		return false;
+	}
+
 }
 
 /*
