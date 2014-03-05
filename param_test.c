d50f1663aca89568e55a0ecea7f4d00c29f49037
Leandros Athanasiadis <leandre84@gmail.com>
2014-03-05 23:06:46 +0100

    params

--- param_test.c
+++ param_test.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
 
-#define MAXPARAMLENGTH 15
+#define MAXPARAMLENGTH 8
 
 #define OPTION_USER "-user"
 #define OPTION_NAME "-name"
@@ -13,12 +14,24 @@
 #define OPTION_PATH "-path"
 
 
-const char params[][MAXPARAMLENGTH] = { OPTION_USER, OPTION_NAME, OPTION_TYPE, OPTION_PRINT, OPTION_LS, OPTION_NOUSER, OPTION_PATH };
-const int paramcnt = sizeof(params)/MAXPARAMLENGTH;
+const char knownparams[][MAXPARAMLENGTH] = { OPTION_USER, OPTION_NAME, OPTION_TYPE, OPTION_PRINT, OPTION_LS, OPTION_NOUSER, OPTION_PATH };
+const int knownparamcnt = sizeof(knownparams)/MAXPARAMLENGTH;
+
+const char *progname = NULL;
+int paramcnt = 0;
 
 int main(int argc, const char * const * argv) {
+
 	int i = 0;
-	for(i=1; i<=paramcnt; i++) {
-		printf("%s\n", params[i-1]);
-	} 
+	for(i=1; i<=knownparamcnt; i++) {
+		printf("%s\n", knownparams[i-1]);
+	}
+
+	progname = basename((char*) argv[0]);
+	paramcnt = argc-1;
+
+	printf("argc: %d\n", argc);
+	printf("paramcnt: %d\n", paramcnt);
+	printf("progname: %s\n", progname);
+
 }
